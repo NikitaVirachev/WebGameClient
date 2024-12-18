f888edff4a78cdf454639c5bc776b8c21b9abcf4
NikitaVirachev <guardsolder@gmail.com>
2024-12-18 23:52:34 +0300

    feat #1: add updatePlayers listening

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 const socket = io();
+
+const players = {};
+
+socket.on("updatePlayers", (backendPlayers) => {
+  // Save players
+  for (const id in backendPlayers) {
+    const backendPlayer = backendPlayers[id];
+
+    if (!players[id]) {
+      players[id] = new Player(backendPlayer.x, backendPlayer.y);
+    }
+  }
+
+  // Delete diconected player when he leaves
+  for (const id in players) {
+    if (!backendPlayers[id]) {
+      delete players[id];
+    }
+  }
+});
